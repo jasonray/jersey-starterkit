37348fafb088ac19461eabef156285277de6ea11
Jason Ray <jason.ray@agilex.com>
2013-01-17 00:55:24 -0500

    added a manual gauge

--- src/main/java/jayray/net/HelloWorldResource.java
+++ src/main/java/jayray/net/HelloWorldResource.java
@@ -8,6 +8,7 @@ import com.yammer.metrics.Metrics;
 import com.yammer.metrics.annotation.Metered;
 import com.yammer.metrics.annotation.Timed;
 import com.yammer.metrics.core.Counter;
+import com.yammer.metrics.core.Gauge;
 
 @Path("hello")
 public class HelloWorldResource {
@@ -19,19 +20,26 @@ public class HelloWorldResource {
 		// by uncommenting the line below, the metrics are outputed to STDOUT
 		// every second
 		// ConsoleReporter.enable(1, TimeUnit.SECONDS);
+
+		Metrics.newGauge(HelloWorldResource.class, "sample-gauge", new Gauge<Integer>() {
+			@Override
+			public Integer value() {
+				return 5;
+			}
+		});
 	}
 
 	@GET
-	@Timed(name="sayHello-timer")
-	@Metered(name="sayHello-meter")
+	@Timed(name = "sayHello-timer")
+	@Metered(name = "sayHello-meter")
 	public String sayhello() {
 		numberOfHellos.inc();
 		return "hello";
 	}
 
 	@GET
-	@Timed(name="echo-timer")
-	@Metered(name="echo-meter")
+	@Timed(name = "echo-timer")
+	@Metered(name = "echo-meter")
 	@Path("echo")
 	public String echo(@QueryParam("message") String message) {
 		numberOfEchos.inc();
