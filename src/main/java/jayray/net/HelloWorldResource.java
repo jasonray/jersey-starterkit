d200f855fd598f97c8f18b96013746186efc9ca6
Jason Ray <jason.ray@agilex.com>
2013-01-16 22:13:13 -0500

    added echo

--- src/main/java/jayray/net/HelloWorldResource.java
+++ src/main/java/jayray/net/HelloWorldResource.java
@@ -2,11 +2,18 @@ package jayray.net;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 
 @Path("hello")
 public class HelloWorldResource {
 	@GET
 	public String sayhello() {
 		return "hello";
+	}
+
+	@GET
+	@Path("echo")
+	public String echo(@QueryParam("message") String message) {
+		return "echo: " + message;
 	}
 }
