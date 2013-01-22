31931f7e179c68699b8564a7b5cac097bdd1ba91
Jason Ray <jasonray@jayray.net>
2013-01-22 07:30:53 -0500

    added a timer

--- src/main/java/jayray/net/Orders/CustomerResource.java
+++ src/main/java/jayray/net/Orders/CustomerResource.java
@@ -1,10 +1,17 @@
 package jayray.net.Orders;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.annotation.Timed;
+import com.yammer.metrics.core.Timer;
+import com.yammer.metrics.core.TimerContext;
 
 @Path("customer")
 public class CustomerResource {
@@ -12,20 +19,41 @@ public class CustomerResource {
 	@GET
 	@Path("id/{id}")
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@Timed(name = "getCustomer")
 	public Customer getCustomer(@PathParam("id") String id) {
-		Customer customer = new Customer();
-		customer.setId(id);
-		customer.setName("Mighty Pulpo");
-		Address address = new Address();
-		address.setCity("austin");
-		address.setState("TX");
-		address.setAddressType("home");
-		customer.getAddresses().add(address);
-		address = new Address();
-		address.setCity("sterling");
-		address.setState("VA");
-		address.setAddressType("work");
-		customer.getAddresses().add(address);
-		return customer;
+		return customerDAL(id);
+	}
+
+	final Timer dalTimer = Metrics.newTimer(this.getClass(), "get-requests", TimeUnit.MILLISECONDS, TimeUnit.SECONDS);
+
+	// handle request
+	public Customer customerDAL(String id) {
+		final TimerContext context = dalTimer.time();
+		try {
+			Customer customer = new Customer();
+			customer.setId(id);
+			customer.setName("Mighty Pulpo");
+			Address address = new Address();
+			address.setCity("austin");
+			address.setState("TX");
+			address.setAddressType("home");
+			customer.getAddresses().add(address);
+			address = new Address();
+			address.setCity("sterling");
+			address.setState("VA");
+			address.setAddressType("work");
+			customer.getAddresses().add(address);
+
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+
+			return customer;
+		} finally {
+			context.stop();
+		}
+
 	}
 }
