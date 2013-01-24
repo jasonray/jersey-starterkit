6ee865c64ebf35c66f044472579e0835edd08e2e
Jason Ray <jasonray@jayray.net>
2013-01-23 22:01:39 -0500

    cleanup duplicate files

--- src/main/java/jayray/net/hello/HelloWorldResource.java
+++ src/main/java/jayray/net/hello/HelloWorldResource.java
@@ -7,11 +7,16 @@ import javax.ws.rs.core.MediaType;
 
 import org.apache.log4j.Logger;
 
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+
 @Path("hello")
+@Api(value = "/hello", description = "say hello")
 public class HelloWorldResource {
 	private static final Logger logger = Logger.getLogger(HelloWorldResource.class);
 
 	@GET
+	@ApiOperation(value = "say hello", notes = "Add extra notes here", responseClass = "java.lang.String")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String sayhello() {
 		logger.debug("sample debug message");
