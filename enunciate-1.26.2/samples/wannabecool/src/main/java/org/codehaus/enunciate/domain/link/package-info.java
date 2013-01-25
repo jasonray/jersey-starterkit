c35e5ea75815d21f86aed9bf8d6cbf336b958aab
Jason Ray <jasonray@jayray.net>
2013-01-25 02:03:05 -0500

    remove enunciate

--- enunciate-1.26.2/samples/wannabecool/src/main/java/org/codehaus/enunciate/domain/link/package-info.java
+++
@@ -1,6 +0,0 @@
-@XmlSchema (
-  namespace = "http://api.example.com/link"
-)
-package org.codehaus.enunciate.domain.link;
-
-import javax.xml.bind.annotation.XmlSchema;
--- enunciate-1.26.2/samples/wannabecool/src/main/java/org/codehaus/enunciate/domain/persona/package-info.java
+++
@@ -1,6 +0,0 @@
-@XmlSchema (
-  namespace = "http://api.example.com/persona"
-)
-package org.codehaus.enunciate.domain.persona;
-
-import javax.xml.bind.annotation.XmlSchema;
