08e223a464d66081e4e0b6f592a83e6ab420da56
Jason Ray <jasonray@jayray.net>
2013-01-26 06:15:02 -0500

    update enunciate icd generation

--- src/main/java/jayray/net/hello/HelloWorldResource.java
+++ src/main/java/jayray/net/hello/HelloWorldResource.java
@@ -5,19 +5,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.apache.log4j.Logger;
-
 @Path("hello")
 public class HelloWorldResource {
-	private static final Logger logger = Logger.getLogger(HelloWorldResource.class);
-
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String sayhello() {
-		logger.debug("sample debug message");
-		logger.info("sample info message");
-		logger.warn("sample warning message");
-		logger.error("sample error message");
 		return "hello";
 	}
 
