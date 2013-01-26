bac3e12b0538f44a57695b7f2ee214c6f0bd1ee2
Jason Ray <jasonray@jayray.net>
2013-01-26 06:42:32 -0500

    removing customer resource from swagger api

--- src/main/java/jayray/net/orders/CustomerResource.java
+++ src/main/java/jayray/net/orders/CustomerResource.java
@@ -6,7 +6,10 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.wordnik.swagger.annotations.Api;
+
 @Path("customer")
+@Api(value = "/customer", description = "This is the customer resource")
 public class CustomerResource {
 
 	@GET
